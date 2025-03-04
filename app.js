b851b23703eb7a6c73e530cb161dce98e31672af
JohnNg-GA <john.q.ng@gmail.com>
2025-03-04 15:35:25 -0800

    1.1 simplified code to reduce redundancy & added like operator functions

--- app.js
+++ app.js
@@ -16,30 +16,32 @@ buttons.forEach((button) => {
         console.log(event.target.innerText)
     })    
 })
+
 calculator.addEventListener('click', (event) => {
     if (event.target.classList.contains('number')) {
         if (display.innerText === '0') {
             display.innerText = display.innerText*1 + event.target.innerText*1
         } else display.innerText = display.innerText + event.target.innerText
     }
-    if (event.target.innerText === '+') {
+    if (event.target.classList.contains('operator')) {
         storedNumber = display.innerText;
-        operator = '+';
-        display.innerText = 0;
-    }
-    if (event.target.innerText === '-') {
-        storedNumber = display.innerText;
-        operator = '-';
-        display.innerText = 0;
-    }
-    if (event.target.innerText === '*') {
-        storedNumber = display.innerText;
-        operator = '*';
-        display.innerText = 0;
-    }
-    if (event.target.innerText === '/') {
-        storedNumber = display.innerText;
-        operator = '/';
+        if (event.target.innerText === '+') {
+            operator = '+';
+        }
+        if (event.target.innerText === '-') {
+            operator = '-';
+        }
+        if (event.target.innerText === '*') {
+            operator = '*';
+        }
+        if (event.target.innerText === '/') {
+            operator = '/';
+        }
+        if (event.target.innerText === 'C') {
+            storedNumber = null;
+            operator = null;
+            display.innerText = 0;
+        }
         display.innerText = 0;
     }
     if (event.target.innerText === '=') {
@@ -55,11 +57,6 @@ calculator.addEventListener('click', (event) => {
         if (operator === '/') {
             display.innerText = storedNumber*1 / display.innerText*1;
         }
-    }
-    if (event.target.innerText === 'C') {
-        storedNumber = null;
-        operator = null;
-        display.innerText = 0;
     }
 })
 /*-------------------------------- Functions --------------------------------*/
